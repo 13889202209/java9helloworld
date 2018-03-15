48c4b21792bd315e2e5f77317e9326d87d7f117c
13889202209 <526477565@qq.com>
2018-03-15 10:41:13 +0800

    456

--- module1/src/com/aa/HelloWorld.java
+++ module1/src/com/aa/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
     private static final Logger logger = Logger.getLogger("helloworld");
 
     public static void main(String[] args) {
-        System.out.println("helloworld123");
+        System.out.println("helloworld456");
         Person p = new Person();
         System.out.println(p.toString());
     }
